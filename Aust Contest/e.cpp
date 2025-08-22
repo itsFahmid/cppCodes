bc05865c65ad178fb896fed68d09c7b73b0e7c15
itsFahmid <fahmidsiddiqueahmed@gmail.com>
2025-08-23 00:36:07 +0600

    solved

--- Aust Contest/e.cpp
+++ Aust Contest/e.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int main(){
-    int y;
-    cin >> y;
+    int y = 53;     // the question said that the input was only the provided data. but the most important point that was added is that "no additional input will be provided" meaning working with cin was a mistake.
     cout << ((((((((y-9)*3)/2)-3)/3)*4)+12)/2) << endl;
 }
