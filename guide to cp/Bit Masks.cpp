e4799493e3471b0fe546e3dd7bcfde6f89dcd361
itsFahmid <fahmidsiddiqueahmed@gmail.com>
2025-08-27 22:54:25 +0600

    bit mask

--- guide to cp/Bit Masks.cpp
+++ guide to cp/Bit Masks.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 int main(){
-    int x = 10, y = 34;
+    int x = 10, y = 35;
 
-    cout << x & y << "\n";
+    cout << (x & y) << "\n";    //Intersection
+    cout << (x | y) << "\n";    //Union
+    cout << (~x) << "\n";       //Compliment
+    cout << (x & ~y) << "\n";   //Difference
 
     for(int k = 31; k >= 0; k--){
         if(x & (1 << k)){
@@ -14,4 +17,10 @@ int main(){
             cout << "0";
         }
     }
+    cout << endl;
+    for (int i = 0; i < 32; i++) {  //prints the elements present in x
+        if (x&(1<<i)) cout << i << " ";
+    }
+    cout << endl;
+    cout << __builtin_popcount(y) << "\n";  //number of 1 present in the bit sequence
 }
