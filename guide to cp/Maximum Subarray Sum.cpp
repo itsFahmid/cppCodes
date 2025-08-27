5b9a982b9fb617bfda2840103febcc2aa7aefa50
itsFahmid <fahmidsiddiqueahmed@gmail.com>
2025-08-27 23:05:14 +0600

    a bit better

--- guide to cp/Maximum Subarray Sum.cpp
+++ guide to cp/Maximum Subarray Sum.cpp
@@ -17,9 +17,22 @@ int worse(int arr[], int n){    // this computes O(n^3) time complexity
     return best;
 }
 
+int barely_works(int arr[], int n){     //this computes O(n^2) time complexity. This was possible by calculating the the sum at the same time when the right end of the subarray moves
+    int best = 0;
+    for (int a = 0; a < n; a++) {
+        int sum = 0;
+        for (int b = a; b < n; b++) {
+            sum += arr[b];
+            best = max(best,sum);
+        }
+    }
+    return n;
+}
+
 int main(){
     int n = 5;
     int arr[n] = {1, 2, -3, 5, 4};
 
     cout << worse(arr, n) << "\n";
+    cout << barely_works(arr, n) << "\n";
 }
