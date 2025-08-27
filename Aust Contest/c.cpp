bcadbc2a555386f43aa1d694de8293f5f54fbe94
itsFahmid <fahmidsiddiqueahmed@gmail.com>
2025-08-27 23:01:28 +0600

    just started on it

--- Aust Contest/c.cpp
+++ Aust Contest/c.cpp
@@ -5,7 +5,11 @@ int main(){
     int t;
     cin >> t;
     while(t--){
-
+        int q, k, l, r;
+        cin >> q >> k >> l >> r;
+        for(int i=0; i<l; i++){
+            if(pow(i, k) < r && )
+        }
     }
 }
 
