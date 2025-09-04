9ad90f7d78af718a9293f962024f6dc4973ae01f
itsFahmid <fahmidsiddiqueahmed@gmail.com>
2025-09-05 02:17:30 +0600

    question

---
+++ STL/Online_A2.cpp
@@ -0,0 +1,14 @@
+/*
+for a delivery service, for each order there is id, the amount in the stack and priority value of the order. we can do these 3 implementation.
+
+add - adds an order.
+order - subtract the given amount of order from the stack.
+top - from the given user input, the order list will be shown based on priority queue.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+}
